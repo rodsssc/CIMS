b991d07f2e75f03947dcf8580918fb22d2b02f57
jhonrods19 <jhonrods2004@gmail.com>
2026-03-12 00:23:28 +0800

    Create server and folder structure

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/", (req, res) => {
+    res.send('SeasdasdRunniasdasng')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
